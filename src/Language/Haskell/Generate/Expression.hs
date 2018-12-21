4a24c571140891a5a49693b253affe327630b6dc
Reuben D'Netto <rdnetto@gmail.com>
2018-12-21 22:10:12 +1100

    Updated to work with haskell-src-exts 1.18.0

--- src/Language/Haskell/Generate/Expression.hs
+++ src/Language/Haskell/Generate/Expression.hs
@@ -1,12 +1,13 @@
-module Language.Haskell.Generate.Expression 
+module Language.Haskell.Generate.Expression
   ( Expression(..)
   , app
   ) where
 
+import Language.Haskell.Exts.SrcLoc
 import Language.Haskell.Exts.Syntax
 
-newtype Expression t = Expression { runExpression :: Exp }
+newtype Expression t = Expression { runExpression :: Exp SrcLoc }
 
 app :: Expression (a -> b) -> Expression a -> Expression b
-app (Expression a) (Expression b) = Expression $ App a b
+app (Expression a) (Expression b) = Expression $ App noLoc a b
 
